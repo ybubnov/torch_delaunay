0a8333672cb5832b302e22a8e1812ba95ecddd11
Yakau Bubnou <girokompass@gmail.com>
2023-01-28 19:09:23 +0300

    Add "circumcenter2d" method to "torchgis".

--- include/torchgis/cartesian.h
+++ include/torchgis/cartesian.h
@@ -2,9 +2,11 @@
 
 #include <torch/torch.h>
 
+
 namespace torchgis {
 namespace cartesian {
 
+torch::Tensor circumcenter2d(const torch::Tensor& input);
 
 torch::Tensor circumradius2d(const torch::Tensor& input);
 
--- src/cartesian.cc
+++ src/cartesian.cc
@@ -1,7 +1,7 @@
 #include <torchgis/cartesian.h>
 
 #include <ATen/Functions.h>
-#include <torch/linalg.h>
+
 
 using namespace torch::indexing;
 
@@ -9,28 +9,35 @@ using namespace torch::indexing;
 namespace torchgis {
 namespace cartesian {
 
-torch::Tensor circumradius2d(const torch::Tensor& input) {
-  // Dimensions:
-  //  0 - triangles
-  //  1 - point (A, B, C)
-  //  2 - x and y coordinate
 
-  auto B = input.index({Slice(), 1}) - input.index({Slice(), 0});
-  auto C = input.index({Slice(), 2}) - input.index({Slice(), 0});
+torch::Tensor circumcenter2d(const torch::Tensor& input) {
+  TORCH_CHECK(input.dim() == 3, "circumcenter2d only supports 3D tensors, got: ", input.dim(), "D");
+
+  auto a = input.index({Slice(), 0});
+  auto b = input.index({Slice(), 1}) - a;
+  auto c = input.index({Slice(), 2}) - a;
 
   std::initializer_list<at::indexing::TensorIndex> x = {Slice(), 0};
   std::initializer_list<at::indexing::TensorIndex> y = {Slice(), 1};
 
-  auto D = 2 * (B.index(x) * C.index(y) - B.index(y) * C.index(x));
+  auto d = 2 * (b.index(x) * c.index(y) - b.index(y) * c.index(x));
 
-  auto B_norm = B.square().sum({1});
-  auto C_norm = C.square().sum({1});
+  auto b_norm = b.square().sum({1});
+  auto c_norm = c.square().sum({1});
 
-  auto Ux = (C.index(y) * B_norm - B.index(y) * C_norm) / D;
-  auto Uy = (B.index(x) * C_norm - C.index(x) * B_norm) / D;
+  auto ux = (c.index(y) * b_norm - b.index(y) * c_norm) / d;
+  auto uy = (b.index(x) * c_norm - c.index(x) * b_norm) / d;
 
-  return (Ux.square() + Uy.square()).sqrt();
+  return at::column_stack({ux, uy}) + a;
 }
+
+
+torch::Tensor circumradius2d(const torch::Tensor& input) {
+  TORCH_CHECK(input.dim() == 3, "circumradius2d only supports 3D tensors, got: ", input.dim(), "D");
+
+  return circumcenter2d(input).square().sum({1}).sqrt();
+}
+
 
 } // cartesian
 } // torchgis
