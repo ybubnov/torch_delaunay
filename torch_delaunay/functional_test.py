9b01df00db186fb818fa72de8bb4897e4f7b522c
Yasha Bubnov <yasha.bubnov@wolt.com>
2024-09-25 21:42:00 +0200

    Copy of the original implementation
    This patch copies flip algorithm from the original implementation.

--- torch_delaunay/functional_test.py
+++ torch_delaunay/functional_test.py
@@ -4,6 +4,8 @@ from torch_delaunay.functional import shull2d
 
 
 def test_shull2d() -> None:
-    points = torch.randn((10, 2), dtype=torch.float64)
+    points = torch.randn((1000, 2), dtype=torch.float64)
     simplices = shull2d(points)
+
     assert simplices.shape[1] == 3
+    assert simplices.shape[0] > 0
