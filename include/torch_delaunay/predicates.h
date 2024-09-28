61252b81ee505bfeebb2a826b5b5ec68508c107e
Yakau Bubnou <girokompass@gmail.com>
2024-09-28 19:20:51 +0200

    Avoid usage of unsqueeze operation (#11)
    This patch tries the best to avoid usage of unsqueeze operation and refactors
    a auxiliary functions to achieve that.

--- include/torch_delaunay/predicates.h
+++ include/torch_delaunay/predicates.h
@@ -20,11 +20,7 @@ incircle2d(
 
 
 bool
-all_clockwise2d(const torch::Tensor& p0, const torch::Tensor& p1, const torch::Tensor& p2);
-
-
-bool
-all_counterclockwise2d(const torch::Tensor& p0, const torch::Tensor& p1, const torch::Tensor& p2);
+incircle2d(const torch::Tensor& points, const torch::Tensor& q);
 
 
 } // namespace torch_delaunay
