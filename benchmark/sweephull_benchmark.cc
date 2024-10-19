7ec81269221e6abf18ad98cabf5553aa5490a54e
Yakau Bubnou <girokompass@gmail.com>
2024-10-19 15:44:36 +0200

    Add benchmark for shull2d (#40)
    This patch adds benchmark for shull2d function.

---
+++ benchmark/sweephull_benchmark.cc
@@ -0,0 +1,43 @@
+/// Copyright (C) 2024, Yakau Bubnou
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <benchmark/benchmark.h>
+
+#include <torch_delaunay.h>
+
+
+using namespace torch_delaunay;
+
+
+static void
+benchmark_shull2d_uniform_float32(benchmark::State& state)
+{
+    auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCPU);
+    auto points = torch::rand({state.range(0), 2}, options);
+
+    for (auto _ : state) {
+        shull2d(points);
+    }
+}
+
+
+BENCHMARK(benchmark_shull2d_uniform_float32)
+    ->Arg(1000)
+    ->Arg(10000)
+    ->Arg(100000)
+    ->Unit(benchmark::kMillisecond);
+
+
+BENCHMARK_MAIN();
